5f65170f2044f49ff240a79f7f6d30c004f086a1
Chris Neale <chris@komododigital.co.uk>
2017-11-21 09:11:03 +0000

    God rays

--- src/smoke.js
+++ src/smoke.js
@@ -31,29 +31,25 @@ ctx.strokeStyle = 'red';
 
 function draw() {
 
-    // ctx.clearRect(0, 0, backlightCanvas.width, backlightCanvas.height);
-
-    ctx.fillStyle = 'grey';
+    ctx.fillStyle = 'rgba(255,255,255,1)';
+    
     ctx.beginPath();
     ctx.rect(0, 0, backlightCanvas.width, backlightCanvas.height);
     ctx.fill();
 
-    ctx.fillStyle = 'white';
-    ctx.beginPath();
-    ctx.arc(document.body.clientWidth/2, document.body.clientHeight/2,50,0,2*Math.PI);
-    ctx.fill();
-
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = 'rgba(255,255,255,.5)';
 
     let pageBoundsMin = document.body.scrollTop;
     let pageBoundsMax = document.body.scrollTop + document.body.clientHeight;
 
     rects.forEach((r) => {
-
+        
         if (r.y+r.height > pageBoundsMin && r.y < pageBoundsMax) {
+            ctx.clearRect(r.x, r.y-document.body.scrollTop, r.width, r.height);
             ctx.beginPath();
             ctx.rect(r.x, r.y-document.body.scrollTop, r.width, r.height);
             ctx.fill();
+
         }
     
     });
@@ -98,37 +94,78 @@ void main() {
 
 
 var fs = `
-
+#ifdef GL_ES
 precision mediump float;
+#endif
 
-uniform float exposure;
-uniform float decay;
-uniform float density;
-uniform float weight;
-uniform vec2 lightPositionOnScreen;
-uniform sampler2D tex;
+uniform vec2 u_resolution;
+uniform float u_time;
 varying vec2 v_texcoord;
-const int NUM_SAMPLES = 100;
+uniform sampler2D u_tex;
 
-void main()
-{
-    vec2 deltaTextCoord = vec2( v_texcoord.st - lightPositionOnScreen.xy );
-    vec2 textCoo = v_texcoord.st;
-    deltaTextCoord *= 1.0 /  float(NUM_SAMPLES) * density;
-    float illuminationDecay = 1.0;
+float random (in vec2 _st) { 
+    return fract(sin(dot(_st.xy,
+                         vec2(12.9898,78.233)))* 
+        43758.5453123);
+}
 
-    for(int i=0; i < NUM_SAMPLES ; i++)
-    {
-        textCoo -= deltaTextCoord;
-        vec4 sample = texture2D(tex, textCoo);
-    
-        sample *= illuminationDecay * weight;
+// Based on Morgan McGuire @morgan3d
+// https://www.shadertoy.com/view/4dS3Wd
+float noise (in vec2 _st) {
+    vec2 i = floor(_st);
+    vec2 f = fract(_st);
 
-        gl_FragColor += sample;
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
 
-        illuminationDecay *= decay;
+    vec2 u = f * f * (3.0 - 2.0 * f);
+
+    return mix(a, b, u.x) + 
+            (c - a)* u.y * (1.0 - u.x) + 
+            (d - b) * u.x * u.y;
+}
+
+#define NUM_OCTAVES 20
+
+float fbm ( in vec2 _st) {
+    float v = 0.0;
+    float a = 0.5;
+    vec2 shift = vec2(100.0);
+    // Rotate to reduce axial bias
+    mat2 rot = mat2(cos(0.5), sin(0.5), 
+                    -sin(0.5), cos(0.50));
+    for (int i = 0; i < NUM_OCTAVES; ++i) {
+        v += a * noise(_st);
+        _st = rot * _st * 2.0 + shift;
+        a *= 0.5;
     }
-    gl_FragColor *= exposure;
+    return v;
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution.xy*3.;
+    // st += st * abs(sin(u_time*0.1)*3.0);
+    vec3 color = vec3(1.0);
+
+    vec2 q = vec2(0.);
+    q.x = fbm( st + 0.00*u_time);
+    q.y = fbm( st + vec2(1.0));
+
+    vec2 r = vec2(0.);
+    r.x = fbm( st + 1.0*q + vec2(1.7,9.2)+ 0.15*u_time );
+    r.y = fbm( st + 1.0*q + vec2(8.3,2.8)+ 0.126*u_time);
+
+    float f = fbm(st+r);
+
+    color = mix(color,
+                vec3(0.666667,0,1),
+                clamp(length(r.x),0.0,1.0));
+
+    gl_FragColor = texture2D(u_tex, v_texcoord) * vec4((f*f*f+.6*f*f+.5*f)*color,1.);
+    gl_FragColor.rgb *= gl_FragColor.a;
 }
 `
 
@@ -150,11 +187,6 @@ function render() {
     gl.useProgram(programInfo.program);
     twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
     twgl.setUniforms(programInfo, {
-        exposure: 1.0,
-        decay: 1.0,
-        density: 1.0,
-        weight: 0.01,
-        lightPositionOnScreen: [0.5,0.5],
         u_resolution: [800,800],
         u_time: u_time += 0.025,
         u_tex: tex,
